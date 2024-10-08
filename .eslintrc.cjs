319693c633f5a52bfa6a676e3bee41f1177df5d6
alex-bulanov.mail <alex-bulanov.mail@yandex.ru>
2024-10-08 23:01:11 +0300

    Первоначальная фиксация

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+	root: true,
+	extends: ['@nuxtjs/eslint-config-typescript', 'prettier'],
+	parserOptions: {
+		warnOnUnsupportedTypeScriptVersion: false
+	},
+	rules: {}
+}
---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	compatibilityDate: '2024-04-03',
+	devtools: { enabled: true },
+	modules: [
+		[
+			'@nuxtjs/eslint-module',
+			{
+				lintOnStart: false,
+				failOnError: true,
+				cache: false
+			}
+		],
+		[
+			'@nuxtjs/stylelint-module',
+			{
+				lintOnStart: false,
+				failOnError: true,
+				cache: false
+			}
+		],
+		'@nuxt/ui'
+	]
+})
