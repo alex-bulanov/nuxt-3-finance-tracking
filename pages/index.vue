5c8e3bf817b66af881c3653a44b8108f75d5833f
alex-bulanov.mail <alex-bulanov.mail@yandex.ru>
2024-10-09 20:56:51 +0300

    Сводный раздел и выбор периода

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { transactionViewOptions } from '~/constants'
+
+const viewSelected = ref(transactionViewOptions[1])
+</script>
 
 <template>
 	<div>
-		<UContainer> Welcome to Finance Tracking </UContainer>
+		<UContainer>
+			<section class="my-10">
+				<div class="flex justify-between items-center">
+					<div>
+						<h2 class="text-xl font-semibold">Summary</h2>
+					</div>
+					<div>
+						<USelectMenu v-model="viewSelected" :options="transactionViewOptions" />
+					</div>
+				</div>
+			</section>
+
+			<section></section>
+
+			<section></section>
+
+			<section></section>
+		</UContainer>
 	</div>
 </template>
