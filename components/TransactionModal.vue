4956c9984db6d045b8d709fa0b6a302ecba2891e
alex-bulanov.mail <alex-bulanov.mail@yandex.ru>
2024-10-15 20:57:38 +0300

    Поля ввода формы

--- components/TransactionModal.vue
+++ components/TransactionModal.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { categories, transactions } from '~/constants'
 interface Props {
 	modelValue: boolean
 }
@@ -24,7 +25,37 @@ const model = computed({
 		<UCard :ui="{ ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
 			<template #header> Создать транзакцию </template>
 
-			<div>hello</div>
+			<div class="grid grid-cols-1 gap-2">
+				<UFormGroup label="Тип транзакции" name="type" :required="true">
+					<USelect :options="transactions" option-attribute="name" />
+				</UFormGroup>
+
+				<UFormGroup label="Сумма" name="amount" :required="true">
+					<UInput size="md" name="amount" type="text" placeholder="" />
+				</UFormGroup>
+
+				<UFormGroup label="Дата" name="created_at" :required="true">
+					<UInput
+						size="md"
+						name="created_at"
+						type="date"
+						icon="i-heroicons-calendar-days-16-solid"
+						placeholder=""
+					/>
+				</UFormGroup>
+
+				<UFormGroup label="Категория" name="category" :required="true">
+					<USelect :options="categories" />
+				</UFormGroup>
+
+				<UFormGroup label="Описание" name="description" hint="Необязательное">
+					<UTextarea size="md" name="description" type="text" placeholder="" />
+				</UFormGroup>
+
+				<div class="py-2">
+					<UButton size="md" type="submit" label="Сохранить" />
+				</div>
+			</div>
 		</UCard>
 	</UModal>
 </template>
--- constants.ts
+++ constants.ts
@@ -3,3 +3,34 @@ export const transactionViewOptions = [
 	'Месяц', // 'Monthly',
 	'День' // 'Daily'
 ]
+
+export const categories = [
+	'Еда', // 'Food',
+	'Автомобиль', // 'Car',
+	'Транспорт', // 'Transport'
+	'Здоровье', // 'Health'
+	'Развлечения', // 'Entertainments'
+	'Образование', // 'Education'
+	'Одежда', // 'Clothes'
+	'Коммунальные', // 'Communal '
+	'Другое' // 'Other'
+]
+
+export const transactions = [
+	{
+		name: 'Доход',
+		value: 'Income'
+	},
+	{
+		name: 'Расходы',
+		value: 'Expenses'
+	},
+	{
+		name: 'Инвестиции',
+		value: 'Investment'
+	},
+	{
+		name: 'Сбережения',
+		value: 'Savings'
+	}
+]
